a589888fc07c7dfc61cd125af4601bd32978a288
trungsqrt <nguyennhumay901@gmail.com>
2025-09-18 11:24:54 +0700

    [Update] Next and packages version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-gir/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   // App Router is enabled by default in Next.js 13+
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
