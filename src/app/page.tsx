a9c28eee2e156d4e3f6736a09f159ca41d726f38
trungsqrt <nguyennhumay901@gmail.com>
2025-09-20 16:18:20 +0700

    update structure code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
 import React from 'react'
-import { HeroSection } from '@/components/sections/hero-section'
-import { TestimonialsSection } from '@/components/sections/testimonials-section'
-import { LearnSection } from '@/components/sections/learn-section'
-import { SocialProofSection } from '@/components/sections/social-proof-section'
-import { PricingSection } from '@/components/sections/pricing-section'
-import { FinalCtaSection } from '@/components/sections/final-cta-section'
-import { ContactSection } from '@/components/sections/contact-section'
+
+import Home from '@/components/Home'
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen">
-      <HeroSection />
-      <TestimonialsSection />
-      <LearnSection />
-      <SocialProofSection />
-      <PricingSection />
-      {/* <FinalCtaSection /> */}
-      <ContactSection />
+    <main>
+      <Home />
     </main>
   )
 }
